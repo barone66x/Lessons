206e2bd91814c9f33985407b2c61c1b17907a827
barone66x <mattia.li.greci@gmail.com>
2023-09-11 17:39:09 +0200

    correct spotify client and library project

---
+++ Spotify prova/Spotify/Program.cs
@@ -0,0 +1,20 @@
+﻿using Spotify.Exercise;
+using System;
+
+namespace Spotify.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DataStore.GetInstance();
+            UserInterface.SourceSelectionInterface();
+
+            UserInterface.Router("h");
+            Console.ReadLine();
+
+
+        }
+    }
+
+}
---
+++ Spotify.Exercise/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify.Exercise
+{
+    internal static class Settings
+    {
+        public static string musicpath = @"C:\Users\mattia.ligreci\Documents\Test\dbCanzoni.csv";
+        public static string radiospath;
+        public static string playlistpath;
+    }
+}
